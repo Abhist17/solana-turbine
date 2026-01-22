6857afc2f51edebb1f8399df22ae0d8259427129
Abhist17 <abhistkamle.170903@gmail.com>
2026-01-22 20:08:56 +0530

    Minted Generug from Bergs Generug machine

--- solana-starter-master/ts/cluster1/nft_image.ts
+++ solana-starter-master/ts/cluster1/nft_image.ts
@@ -1,30 +1,37 @@
-import wallet from "./wallet/turbin3-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
-import { readFile } from "fs/promises"
+import wallet from "./wallet/turbin3-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+    createGenericFile,
+    createSignerFromKeypair,
+    signerIdentity,
+} from "@metaplex-foundation/umi";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import { readFile } from "fs/promises";
 
 // Create a devnet connection
-const umi = createUmi('https://api.devnet.solana.com');
+const umi = createUmi("https://api.devnet.solana.com");
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 
-umi.use(irysUploader());
+umi.use(irysUploader({ address: "https://devnet.irys.xyz" }));
 umi.use(signerIdentity(signer));
 
 (async () => {
     try {
         //1. Load image
+        const data = await readFile('/home/ravenn/Desktop/solana_turbine/solana-starter-master/generug.png');
+        
         //2. Convert image to generic file.
+        const file = createGenericFile(data, "generug.png", {
+            contentType: "image/png",
+        });
+
         //3. Upload image
-
-        // const image = ???
-
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
-    }
-    catch(error) {
+        const [myUri] = await umi.uploader.upload([file]);
+        
+        console.log("Your image URI: ", myUri);
+    } catch (error) {
         console.log("Oops.. Something went wrong", error);
     }
 })();
